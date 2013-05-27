ae2d87c35cabf32f6aba52c40d205a9e7668bc4e
Dan Vera <dan@yovu.co>
2013-05-27 16:09:19 -0400

    ?

--- padeploy.sh
+++ padeploy.sh
@@ -11,6 +11,14 @@ if [ -f "/etc/redhat-release" ]; then
 				read -p "MySQL Password: " mysqlPassword
 				read -p "Retype password: " mysqlPasswordRetype
 
+				yum install -y httpd php php-mysql php-gd php-imap php-ldap php-mbstring php-odbc php-pear php-xml php-xmlrpc php-pecl-apc mysql mysql-server
+
+				chkconfig mysql-server on
+				chkconfig httpd on
+
+				chkconfig --levels 235 mysqld on
+				/etc/init.d/mysqld restart
+
 				while [[ "$mysqlPassword" = "" && "$mysqlPassword" != "$mysqlPasswordRetype" ]]; do
 					  echo -n "Please enter the desired mysql root password: "
 					  stty -echo
@@ -24,6 +32,10 @@ if [ -f "/etc/redhat-release" ]; then
 					    	echo "Passwords do not match!"
 					  fi
 				done
+
+				/usr/bin/mysqladmin -u root password $mysqlPassword
+
+				echo 'Okay.... apache, php and mysql is installed, running and set to your desired password'
 				
 
             elif [[ $i = "Fedora" ]]; then
