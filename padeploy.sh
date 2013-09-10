478f9e1e6d7921ecb854c1a93c3b58af177ba1be
Dan Vera <dan@yovu.co>
2013-09-10 09:11:04 -0400

    ohh

--- padeploy.sh
+++ padeploy.sh
@@ -109,7 +109,7 @@ server {
         root           /usr/share/nginx/html;
         fastcgi_pass   127.0.0.1:9000;
         fastcgi_index  index.php;
-        fastcgi_param  SCRIPT_FILENAME   $document_root$fastcgi_script_name;
+        fastcgi_param  SCRIPT_FILENAME   \$document_root\$fastcgi_script_name;
         include        fastcgi_params;
     }
 }
