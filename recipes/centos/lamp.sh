084a7ecb8a9befe02837758bc0370b05a1f6a612
Dan Vera <dan@ianalyst.com>
2013-12-02 17:01:39 -0500

    lamp baby

--- recipes/centos/lamp.sh
+++ recipes/centos/lamp.sh
@@ -6,12 +6,14 @@ echo '------------------------'
 read -p "MySQL Password: " mysqlPassword
 read -p "Retype password: " mysqlPasswordRetype
 
-yum install -y httpd php mysql mysql-server
+yum install -y httpd php php-mysql php-gd php-imap php-ldap php-mbstring php-odbc php-pear php-xml php-xmlrpc php-pecl-apc mysql mysql-server
 
-chkconfig mysql-server on
-chkconfig httpd on
+chkconfig --levels 235 mysqld on
+/etc/init.d/mysqld start
 
-/etc/init.d/mysqld restart
+chkconfig --levels 235 httpd on
+/etc/init.d/httpd start
+/etc/init.d/httpd restart
 
 while [[ "$mysqlPassword" = "" && "$mysqlPassword" != "$mysqlPasswordRetype" ]]; do
   echo -n "Please enter the desired mysql root password: "
@@ -29,5 +31,27 @@ done
 
 /usr/bin/mysqladmin -u root password $mysqlPassword
 
-echo $mysqlPassword
-echo 'Okay.... apache, php and mysql is installed, running and set to your desired password'
+echo "###############################################"
+echo "#                                             #"
+echo "#                 That's it!                  #"
+echo "#          LAMP has been installed!           #"
+echo "#                                             #"
+echo "#                                             #"
+echo "#    Apache:                                  #"
+echo "#    -------------------------------------    #"
+echo "#    location: /var/www/html                  #"
+echo "#                                             #"
+echo "#                                             #"
+echo "#    MySQL:                                   #"
+echo "#    -------------------------------------    #"
+echo "#    username: root                           #"
+echo "#    password: $mysqlPassword                 #"
+echo "#                                             #"
+echo "#    -------------------------------------    #"
+echo "#                                             #"
+echo "#    Make sure to save the MySQL password.    #"
+echo "#                                             #"
+echo "#                                             #"
+echo "###############################################"
+
+break
